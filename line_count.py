963056d1dd71b6c27627b71a8e8a1f4c959aa4eb
Brody Broughton <broughton.brody07@gmail.com>
2024-09-03 15:44:31 -0600

    First Commit Test

--- line_count.py
+++ line_count.py
@@ -1,4 +1,2 @@
 def line_count():
-  """
-  Reference the "Reading Files in Python" lesson to open the `file.txt` file, count the number of words in the file, and return the count.
-  """
+    print('he')
