af418baad8f952fc684a7b713654dd2e3f7ce2d5
Brody Broughton <broughton.brody07@gmail.com>
2024-09-04 14:48:59 -0600

    Final Line Count Commit

--- line_count.py
+++ line_count.py
@@ -1,11 +1,23 @@
 def line_count():
+    
+    # Opens file.txt in read mode
     f = open("file.txt", "r")
+    
+    # Count variable for the number of lines
     count = 0
+    
+    # Lines variable for the list of the file
     lines = f.readlines()
     
+    # For loops that goes through each line, strips it, and adds 1 to the count
     for line in lines:
+        
         line.strip()
+        
         count += 1 
 
+    # Closes file
     f.close()
+    
+    # Returns the number of lines :)
     return count
